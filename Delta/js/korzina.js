6c808a7544e04fefdbeec96db5bb5cc5144d7d99
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2024-06-05 10:49:58 +0400

    AION, Avtokovry, DarkGames, Delta, Apetito
    AION, Avtokovry, DarkGames, Delta, Apetito

---
+++ Delta/js/korzina.js
@@ -0,0 +1,16 @@
+<script type="text/javascript"> 
+$(document).ready(function(e) {
+	$("#korzina").click(function(){
+		$(".overlay").css("display","block");
+		$(".wrap-korzina-page").css("display","block")});		
+	$("input.btn-buy").click(function(){
+		$(".overlay").css("display","block");
+		$(".wrap-korzina-page").css("display","block")});		
+	$("#close").click(function(){
+		$(".overlay").css("display","none");
+		$(".wrap-korzina-page").css("display","none")});
+	$("#continue").click(function(){
+		$(".overlay").css("display","none");
+		$(".wrap-korzina-page").css("display","none")});
+});
+</script>
