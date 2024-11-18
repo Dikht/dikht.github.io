732aad2ad1d08a3c70dec6541798295f8b3d08c9
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2024-11-18 18:12:42 +0400

    commit
    Adobe added
    Delta fix

---
+++ Adobe/js/script.js
@@ -0,0 +1,14 @@
+$(".slider").slick({
+  dots: false,
+	arrows: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000, 
+	draggable:true,
+	swype: true,
+	variableWidth: true,
+	rows:2,
+});
